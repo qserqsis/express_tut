ded65a039b86ee715b59eac30f43a00ad857327a
Vyacheslav Trubetskoy <qserqsis@yandex.ru>
2018-12-13 14:56:52 +0700

    add server.js, add test fuctional

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require ('express');
+
+var app = express();
+
+var artists = [
+    {
+        id:1,
+        name:'Noize MC'
+    },
+    {
+        id:2,
+        name:'Linkin Park'
+    },
+    {
+        id:3,
+        name:'Queen'
+    },
+];
+
+app.get ('/', function(req, res){
+    res.send('Hello API');
+})
+
+app.get('/artists', function(req, res){
+    res.send(artists)
+    console.log('Artists were sent')
+})
+
+app.listen(3012, function(){
+    console.log('API app started');
+})
+
